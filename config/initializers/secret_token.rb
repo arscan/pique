27b8499392e19de6bfe9688abbfa280b64454f23
Rob Scanlon <robscanlon@gmail.com>
2013-05-21 21:14:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pique::Application.config.secret_token = '88b9a4d4f0168fb6418976017204ec3c6e1f932859add08a73dc97583ca712941b0a952018fbd4a7a6d85aee2f332045e9bb987d7008f6f8432b35cb3e07d9e7'
